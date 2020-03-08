7e1b91a6b3a6414585bc9bbd6f2b2d13787a05d3
pratheekbhandary <pratheekbhandary@github.com>
2020-03-09 02:25:10 +0530

    Updates

---
+++ precache-manifest.5fedb1cea8ae3af919a4c199a3508799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0daf39950594bf2cb38122710dfcd0ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423fe9faae99cf91da2b",
+    "url": "/static/css/main.72493591.chunk.css"
+  },
+  {
+    "revision": "0f4d6c823240e55c61ff",
+    "url": "/static/js/2.7245b878.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7245b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423fe9faae99cf91da2b",
+    "url": "/static/js/main.9e9f8a54.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  }
+]);
